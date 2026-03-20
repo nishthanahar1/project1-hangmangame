1e789a0c9776d1ed8b81ee2fb0a5b41ebba8b5f7
Nishtha <naharnishtha@gmail.com>
2026-03-20 12:12:02 +0530

    Added Hangman Game Visual

--- hangmangame.py
+++ hangmangame.py
@@ -1,16 +1,95 @@
 import random
 
+# Hangman stages (animation)
+stages = [
+    """
+     -----
+     |   |
+     O   |
+    /|\  |
+    / \  |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\  |
+    /    |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|\  |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+    /|   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+     |   |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+     O   |
+         |
+         |
+         |
+    --------
+    """,
+    """
+     -----
+     |   |
+         |
+         |
+         |
+         |
+    --------
+    """
+]
+
+# Word list
 words = ["apple", "tiger", "chair", "plant", "bread"]
 word = random.choice(words)
 
+# Game variables
 guessed_word = ["_"] * len(word)
 attempts = 6
+guessed_letters = []
 
 print("Welcome to Hangman!")
 print(" ".join(guessed_word))
+print(stages[attempts])
 
+# Game loop
 while "_" in guessed_word and attempts > 0:
     guess = input("Guess a letter: ").lower()
+
+    # Prevent repeated guesses
+    if guess in guessed_letters:
+        print("You already guessed that letter!")
+        continue
+
+    guessed_letters.append(guess)
 
     if guess in word:
         print("Correct guess!")
@@ -21,10 +100,12 @@ while "_" in guessed_word and attempts > 0:
         attempts -= 1
         print("Wrong guess! Attempts left:", attempts)
 
+    print(stages[attempts])  # show hangman
     print(" ".join(guessed_word))
 
 # Final result
 if "_" not in guessed_word:
     print("🎉 You won! Word was:", word)
 else:
+    print(stages[0])
     print("😢 You lost! Word was:", word)
